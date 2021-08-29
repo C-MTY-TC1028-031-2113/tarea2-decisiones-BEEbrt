acf6c3e029b579e2c2b7e5bec2a2b880d2feca97
BEEbrt <A01746987@itesm.mx>
2021-08-29 17:11:37 -0500

    cuadrantes

--- assignments/04Cuadrante/src/exercise.py
+++ assignments/04Cuadrante/src/exercise.py
@@ -1,6 +1,11 @@
-def main():
-    # Escribe tu código abajo de esta línea
-    pass
-
-if __name__ == '__main__':
-    main()
+grados= float(input(" Introduce los grados: "))
+if grados == 0:
+        print ("Sin cuadrante")
+elif grados >= 1 and grados <= 90:
+        print ("Primer cuadrante")
+elif grados >= 91 and grados <= 180:
+        print ("Segundo cuadrante")
+elif grados >=181 and grados <= 270:
+        print ("Tercer cuadrante")
+elif grados >=271 and grados <= 360:
+        print ("Cuarto cuadrante")
