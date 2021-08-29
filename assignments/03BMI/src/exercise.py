901e3ad71e7c2b6bb646cbf936a3c8ad959f4230
BEEbrt <A01746987@itesm.mx>
2021-08-29 16:18:59 -0500

    BMI

--- assignments/03BMI/src/exercise.py
+++ assignments/03BMI/src/exercise.py
@@ -1,6 +1,14 @@
-def main():
-    #escribe tu código abajo de esta línea
-    pass
-
-if __name__=='__main__':
-    main()
+peso = float (input("Su peso en kilogramos por favor : "))
+altura = float(input ("Su altura en metros por favor: "))
+IMC = peso / altura**2
+print("IMC: " + str(IMC) )
+if IMC<20:
+        print ("Peso Bajo")
+elif IMC <= 20 and IMC <= 25:
+        print ("Normal")
+elif IMC <= 25 and IMC <= 30:
+        print ("Sobrepeso")
+elif IMC <= 30 and IMC <= 40:
+        print ("Obesidad")
+elif IMC >= 40.00:
+        print ("Obesidad Morbida")
