d058bf8e64936764a3a8f42b902c5328323b3d2b
BEEbrt <A01746987@itesm.mx>
2021-08-29 15:26:43 -0500

    tramite de licencia

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,7 +1,10 @@
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+edad= int(input("Ingresa tu edad: "))
+if edad>=18:
+ print("Eres mayor de edad ")
+ id= (input("¿Tienes identificacion oficial?"))
+ if id=="si":
+        print("Tramite de licencia concedido ")
+ else:
+    print("tramite de licencia denegado")
+else:
+    print("No cumples con los requisitos")
