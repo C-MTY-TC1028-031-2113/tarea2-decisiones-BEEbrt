8e1886e843288795673f31b1926df56765319f1b
BEEbrt <A01746987@itesm.mx>
2021-08-29 15:54:04 -0500

    ordena

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -1,10 +1,28 @@
-def main():
-    # Escribe el código adecuado para completar el programa
-    x = int(input("Ingresa el primer número: "))
-    y = int(input("Ingresa el segundo número: "))
-    z = int(input("Ingresa el tercer número: "))
-    
-    print("Hola")
+num1= int(input("Ingresa el primer número: "))
+num2= int(input("Ingresa el segundo número: "))
+num3= int(input("Ingresa el tercer número: "))
 
-if __name__=='__main__':
-    main()
+if num1<=num2 and num2<=num3:
+    print(num1)
+    print(num2)
+    print(num3)
+elif num1<=num3 and num3<=num2:
+    print(num1)
+    print(num3)
+    print(num2)
+elif num2<=num3 and num3<=num1:
+    print(num2)
+    print(num3)
+    print(num1)
+elif num2<=num1 and num1<=num3:
+    print(num2)
+    print(num1)
+    print(num3)
+elif num3<=num1 and num1<=num2:
+    print(num3)
+    print(num1)
+    print(num2)
+else:
+    print(num3)
+    print(num2)
+    print(num1)
